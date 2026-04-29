93707d1febafe288c92e729f4ae5066efb6e80d1
josepetruciojr <josepetruciodev@gmail.com>
2026-04-29 16:44:24 -0300

    fix: Conserta o construtor de RentalService

--- src/main/java/application/ProgramRentCar.java
+++ src/main/java/application/ProgramRentCar.java
@@ -14,17 +14,16 @@ public class ProgramRentCar {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-
         Scanner sc = new Scanner(System.in);
 
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         System.out.println("Entre com os dados do aluguel");
         System.out.print("Modelo do carro: ");
         String carModel = sc.nextLine();
-        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retirada (dd/MM/yyyy HH:mm): ");
         LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retorno (dd/MM/yyyy HH:mm): ");
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
         CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
@@ -34,7 +33,7 @@ public class ProgramRentCar {
         System.out.print("Entre com o preço por dia: ");
         double pricePerDay = sc.nextDouble();
 
-        RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
 
         rentalService.processInvoice(cr);
 
@@ -42,8 +41,6 @@ public class ProgramRentCar {
         System.out.println("Pagamento basico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
         System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
         System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
-
-        sc.close();
 
     }
 }
--- src/main/java/services/RentalService.java
+++ src/main/java/services/RentalService.java
@@ -19,17 +19,19 @@ public class RentalService {
     }
 
     public void processInvoice(CarRental carRental){
-
-        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toHours();
-        double hours = minutes / 60;
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
 
         double basicPayment;
-        if (hours <= 12.0){
+        if (hours <= 12.0) {
             basicPayment = pricePerHour * Math.ceil(hours);
         }
-            else{
-                basicPayment = pricePerDay * Math.ceil(hours /24.0 );
+        else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24);
         }
-        carRental.setInvoice(new Invoice(50.0, 10.0));
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
     }
 }
